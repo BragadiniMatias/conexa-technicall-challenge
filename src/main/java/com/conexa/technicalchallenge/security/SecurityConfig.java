c05550435f86ec3884981265b55039b681476e2a
BragadiniMatias <matias.bragadini@gmail.com>
2025-05-04 18:03:38 -0300

    Fix cors

--- src/main/java/com/conexa/technicalchallenge/security/SecurityConfig.java
+++ src/main/java/com/conexa/technicalchallenge/security/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and()
+        http.cors().configurationSource(corsConfigurationSource()).and()
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers(
