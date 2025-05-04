bc2c52121954c588ba4d7933bf8720ca7991eee9
BragadiniMatias <matias.bragadini@gmail.com>
2025-05-04 18:55:18 -0300

    Agrego profiles para poder usaw swagger deployado

--- src/main/java/com/conexa/technicalchallenge/config/SwaggerConfig.java
+++ src/main/java/com/conexa/technicalchallenge/config/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package com.conexa.technicalchallenge.config;
 
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.OpenAPI;
@@ -10,13 +12,17 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 @Configuration
 public class SwaggerConfig {
 
+    @Value("${swagger.server-url}")
+    private String swaggerServerUrl;
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
                         .title("Conexa API")
                         .version("1.0")
-                        .description("API documentation for Conexa tech hallenge"))
+                        .description("API documentation for Conexa tech challenge"))
+                .addServersItem(new Server().url(swaggerServerUrl))
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                 .components(new io.swagger.v3.oas.models.Components()
                         .addSecuritySchemes("bearerAuth", new SecurityScheme()
