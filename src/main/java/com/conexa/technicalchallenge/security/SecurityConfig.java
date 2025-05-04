08536507fbca8ffba031fe8c2723bad033347a38
BragadiniMatias <matias.bragadini@gmail.com>
2025-05-04 18:21:53 -0300

    Test cors config

--- src/main/java/com/conexa/technicalchallenge/security/SecurityConfig.java
+++ src/main/java/com/conexa/technicalchallenge/security/SecurityConfig.java
@@ -77,9 +77,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Collections.singletonList("https://unchanged-karlie-matiasbragadini-ef7121c6.koyeb.app"));
+        config.setAllowedOrigins(Collections.singletonList("*"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(Collections.singletonList("*"));
+        config.setAllowedOrigins(Collections.singletonList("*"));
         config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
